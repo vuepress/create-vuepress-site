1478760b4b068186d1ff057150c6958d7514dea9
Ben Hong <ben@bencodezen.io>
2020-08-21 22:05:16 -0400

    chore: copy over files from create-vuepress

---
+++ cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const {existsSync} = require('fs');
+const {join} = require('path');
+const yParser = require('yargs-parser');
+const semver = require('semver');
+const chalk = require('chalk');
+const run = require('./lib/run');
+
+// Print version and @local
+const args = yParser(process.argv.slice(2));
+
+if (args.v || args.version) {
+	console.log(require('./package').version);
+	if (existsSync(join(__dirname, '.local'))) {
+		console.log(chalk.cyan('@local'));
+	}
+	process.exit(0);
+}
+
+if (!semver.satisfies(process.version, '>= 8.0.0')) {
+	console.error(
+		chalk.red('✘ The generator will only work with Node v8.0.0 and up!')
+	);
+	process.exit(1);
+}
+
+const name = args._[0] || '';
+run({
+	name,
+	args
+});
